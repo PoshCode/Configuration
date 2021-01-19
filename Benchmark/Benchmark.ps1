5c9753aef111f775138a542802ef46a89d468b7d
Joel Bennett <Jaykul@HuddledMasses.org>
2021-01-18 23:14:34 -0500

    Update the benchmark so it run on all versions available
    So we can easily benchmark against previous versions available on our box.

--- Benchmark/Benchmark.ps1
+++ Benchmark/Benchmark.ps1
@@ -1,12 +1,27 @@
+[CmdletBinding()]
+param([int]$Count = 10)
 $dataPath = Join-Path $PSScriptRoot "../Benchmark/Data/Configuration.psd1"
 
-Write-Warning "The following Export-ModuleMember error can be ignored."
-Get-ChildItem -Recurse -Filter *.ps1 -Path (Join-Path $PSScriptRoot "../Source/Metadata/") | ForEach-Object { . $_ }
+foreach ($Version in Get-Module Configuration -ListAvailable | Sort-Object Version) {
+    Remove-Module Configuration -Force
+    Import-Module $Version.Path -Force
+    $Configuration = Get-Module Configuration
+    Write-Host "${fg:white}$($Configuration.Name) v$($Configuration.Version)$(if(($pre=$Configuration.PrivateData.PSData.PreRelease)) {"-$pre"})"
 
-$inputObject = Get-Content -Raw $dataPath | ConvertFrom-Metadata
+    $ToTime = [System.Collections.Generic.List[timespan]]::new()
+    $FromTime = [System.Collections.Generic.List[timespan]]::new()
 
-$timingConvertTo = Measure-Command {
-  ConvertTo-Metadata -InputObject $inputObject
+    $Timer = [System.Diagnostics.Stopwatch]::new()
+    for ($i=0;$i -lt $Count; $i++) {
+        $Timer.Restart()
+        $inputObject = ConvertFrom-Metadata -InputObject (Get-Content -Raw $dataPath)
+        $FromTime.Add($Timer.Elapsed)
+        $outputObject = ConvertTo-Metadata -InputObject $inputObject
+        $ToTime.Add($Timer.Elapsed)
+    }
+    $From = $FromTime | Measure-Object TotalMilliseconds -Sum -Average -Maximum -Minimum
+    $To = $ToTime | Measure-Object TotalMilliseconds -Sum -Average -Maximum -Minimum
+
+    Write-Host "  ${fg:grey}ConvertTo-Metadata completed in ${fg:Cyan}$($To.Average) milliseconds${fg:grey} on average. Max $($To.Maximum) to $($To.Minimum) minimum."
+    Write-Host "  ${fg:grey}ConvertFrom-Metadata completed in ${fg:Cyan}$($From.Average) milliseconds${fg:grey} on average. Max $($From.Maximum) to $($From.Minimum) minimum."
 }
-
-Write-Host "ConvertTo-Metadata completed in $($timingConvertTo.Milliseconds) milliseconds."
